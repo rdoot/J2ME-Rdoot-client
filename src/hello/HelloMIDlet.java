bc4388161d0264a4ad846627e9d0f55afb2b7876
Arjo Chakravarty <arjo129@gmail.com>
2013-03-31 10:50:24 +0800

    Slimmed down bluetooth code, Client Ready

--- src/hello/HelloMIDlet.java
+++ src/hello/HelloMIDlet.java
@@ -1,11 +1,17 @@
 package hello;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.*;
+import javax.bluetooth.*;
 import javax.microedition.lcdui.*;
 import javax.microedition.media.*;
 import javax.microedition.media.control.*;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.media.control.VideoControl;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
 public class HelloMIDlet extends MIDlet implements CommandListener, Runnable {
 
     private Display display;
@@ -14,29 +20,25 @@ public class HelloMIDlet extends MIDlet implements CommandListener, Runnable {
     private Player player;
     private VideoControl videoControl;
     private Video video;
-    private Client server;
-    private Bluetooth bt;
-    private String msg;
-    private String[] clients = new String[0];
-    final String SERVICE_NAME = "rdootROV1";
-final int STATE_NOT_CONNECTED = 0;
-final int STATE_CONNECTING = 1;
-final int STATE_CONNECTED = 2;
-final int STATE_SENDING = 3;
-final int STATE_SENT = 4;
-final int STATE_PIC_TAKEN = 5;
-private int state = STATE_NOT_CONNECTED;
+    private UUID HELLOSERVICE_ID;
+    private DiscoveryAgent mDiscoveryAgent;
+    private String url = null;
+    private int state = 0;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private StreamConnection conn;
+
     public HelloMIDlet() {
 
         exit = new Command("Exit", Command.EXIT, 0);
         camera = new Command("Camera", Command.SCREEN, 0);
         back = new Command("Back", Command.BACK, 0);
         capture = new Command("Capture", Command.SCREEN, 0);
-
+        HELLOSERVICE_ID = new UUID("0000110100001000800000805F9B34FB",false);
         form = new Form("Capture Video");
         form.addCommand(camera);
         form.setCommandListener(this);
-        bt = new Bluetooth(this); // RFCOMM
+      
     }
 
     public void startApp() {
@@ -82,64 +84,51 @@ private int state = STATE_NOT_CONNECTED;
 
    
 
-    void enqueueLibraryEvent(Bluetooth aThis, int event, Object data) {
-        switch (event) {
-    case Bluetooth.EVENT_DISCOVER_DEVICE:
-      msg = "Found device at: " + ((Device) data).address + "...";
-      break;
-    case Bluetooth.EVENT_DISCOVER_DEVICE_COMPLETED:
-      //msg = "Found " + length((Device[]) data) + " devices, looking for service " + SERVICE_NAME + "...";
-      break;
-    case Bluetooth.EVENT_DISCOVER_SERVICE:
-      msg = "Found Service " + ((Service[]) data)[0].name + "...";
-      break;
-    case Bluetooth.EVENT_DISCOVER_SERVICE_COMPLETED:
-      Service[] services = (Service[]) data;
-
-      for (int i=0; i<services.length; i++) {
-        if (services[i].name.equals(SERVICE_NAME)) {
-          server = services[i].connect();
-          msg = "Server connected";
-          state = STATE_CONNECTED;
-          return;
-        }
-      }
-
-      msg = "Search complete, Server not found \nAny key to retry.";
-      state = STATE_NOT_CONNECTED;
-      break;
-    }
-    }
-
-    void enqueueLibraryEvent(Bluetooth aThis, int EVENT_DISCOVER_DEVICE_COMPLETED, Device[] devices) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    void enqueueLibraryEvent(Bluetooth bt, int EVENT_DISCOVER_SERVICE_COMPLETED) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+ 
 
     public void run() {
-        try {
-                byte[] raw = videoControl.getSnapshot(null);
+       try {
+               byte[] raw = videoControl.getSnapshot(null);
+               if(state==0){
+                   try {
+                    mDiscoveryAgent = LocalDevice.getLocalDevice().getDiscoveryAgent();
+                    url = mDiscoveryAgent.selectService(HELLOSERVICE_ID, ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+                    conn = (StreamConnection) Connector.open(url);
+                    in = new DataInputStream(conn.openInputStream());
+                    out = new DataOutputStream(conn.openOutputStream());
+                    state = 1;
+                   }catch (Exception e){ state = 0;}
+               }
+               else if (state == 1){
+                   try {
+                       out.writeInt(raw.length);
+                       String received = in.readUTF();
+                       if(received == "Ready"){
+                            out.write(raw);
+                            received = in.readUTF();
+                            if(received == "left"){
+                                //Turn Left
+                            }
+                            if(received == "right"){
+                                //Turn Right
+                            }
+                            if(received == "go"){
+                                //Forward
+                            }
+                            if(received == "quiting"){
+                                conn.close();
+                                state = 0;
+                            }
+                       }
+                   }catch(Exception e){
+
+                   }
+               }
 
 
-                //display.setCurrent(form);
-                if (state == STATE_CONNECTED) {
-                     state=STATE_NOT_CONNECTED; // setting it back if sending works
-                     try{
-                          server.writeInt(raw.length);
-                          server.write(raw);
-                          state = STATE_SENDING;
-                        }
-                    catch (Exception e) {
-                    server.stop();
-                    msg = "connection lost";
-                }
-                } }
-                catch(MediaException e){
-
-                }
+       }
+       catch (Exception e){}//display.setCurrent(form);
+                
     }
 
     class Video extends Thread {
@@ -158,28 +147,15 @@ private int state = STATE_NOT_CONNECTED;
                 byte[] raw = videoControl.getSnapshot(null);
                
                 
-                //display.setCurrent(form);
-                if (state == STATE_CONNECTED) {
-                     state=STATE_NOT_CONNECTED; // setting it back if sending works
-                     try{
-                          server.writeInt(raw.length);
-                          server.write(raw);
-                          state = STATE_SENDING;
-                        }
-                    catch (Exception e) {
-                    server.stop();
-                    msg = "connection lost";
-                }
-               // player.close();
-               // player = null;
-               // videoControl = null;
+
             
-                }
+                
             }
             catch(Exception e){
             }
 
         }
     };
+
 }
 
